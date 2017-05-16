29e7a763576a71384506afc295924c9c9b292e78
yukiiris <25852469@qq.com>
2017-05-16 21:33:59 +0800

    实现注册功能

---
+++ Shop/src/com/shop/servlet/RegisterServlet.java
@@ -0,0 +1,70 @@
+package com.shop.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shop.dao.factory.DAOFactory;
+import com.shop.vo.User;
+
+public class RegisterServlet extends HttpServlet {
+
+	public void doGet(HttpServletResponse response, HttpServletRequest request)
+	throws ServletException, IOException
+	{
+		String path = "register.jsp";
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		List<String> info = new ArrayList<String>();
+		
+		if (name == null || "".equals(name))
+		{
+			info.add("用户名不能为空");
+		}
+		else if (name.length() > 9)
+		{
+			info.add("用户名过长");
+		}
+		if (password == null || "".equals(password))
+		{
+			info.add("密码不能为空");
+		}
+		else if (password.length() > 9)
+		{
+			info.add("密码过长");
+		}
+		
+		if (info.size() == 0)
+		{
+			User user = new User();
+			user.setName(name);
+			user.setPassword(password);
+			try
+			{
+				if (DAOFactory.getIUserDAOInstance().doCreate(user))
+				{
+					info.add("创建成功");
+				}
+				else
+				{
+					info.add("创建失败");
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void doPost(HttpServletResponse response, HttpServletRequest request)
+	throws ServletException, IOException
+	{
+		doGet(request, response);
+	}
+}
