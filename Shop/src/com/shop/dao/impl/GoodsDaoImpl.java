530ed465cbe987594fa7da45baf6c8ebcd11ba84
yukiiris <25852469@qq.com>
2017-05-17 21:57:58 +0800

    写物品类中

---
+++ Shop/src/com/shop/dao/impl/GoodsDaoImpl.java
@@ -0,0 +1,10 @@
+package com.shop.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class GoodsDaoImpl {
+
+	private Connection conn;
+	private PreparedStatement pstm;
+}
---
+++ Shop/src/com/shop/vo/Goods.java
@@ -0,0 +1,38 @@
+package com.shop.vo;
+
+public class Goods {
+
+	private String name;
+	private String introduction;
+	private float price;
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getIntroduction()
+	{
+		return introduction;
+	}
+	
+	public void setIntroduction(String introduction)
+	{
+		this.introduction = introduction;
+	}
+	
+	public float getPrice()
+	{
+		return price;
+	}
+	
+	public void setPrice(float price)
+	{
+		this.price = price;
+	}
+}
