2095c65dbaffdb46b03b4e2f9512f87315f82040
yukiiris <25852469@qq.com>
2017-05-25 14:47:47 +0800

    转移啦

--- Shop/src/com/shop/dao/IUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.shop.dao;
-
-import com.shop.vo.User;
-
-public interface IUserDAO {
-
-	public boolean doCreate(User user) throws Exception;
-	public boolean findUser(User user) throws Exception; 
-}
